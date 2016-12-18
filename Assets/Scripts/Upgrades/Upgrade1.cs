1b0aad6c0c7f848bfad458454b653cd588ca6062
Kaj <kaaiman99@hotmail.com>
2016-12-18 21:21:17 +0100

    Werk aub

--- Assets/Scripts/Upgrades/Upgrade1.cs
+++ Assets/Scripts/Upgrades/Upgrade1.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Upgrade1 : UpgradeMain {
-
-	void Start () {
+	
+	override protected void Start () {
 		
 	}
 	
-	void Update () {
-		
+	override protected void onClick(){
+
 	}
+
 }
